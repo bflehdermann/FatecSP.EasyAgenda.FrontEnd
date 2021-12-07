07b0f9f8864cf80f54f3dd4ffdb9a3170eb5c46e
Danillo Felipe Aragão <danillo2aragao@gmail.com>
2021-12-06 21:33:25 -0300

    .

--- src/components/API/index.js
+++ src/components/API/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: `http://localhost:3500/api/`
+  baseURL: process.env.baseDev
 });
 
 const token = localStorage.getItem('token')
